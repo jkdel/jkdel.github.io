6d234d004db6dd100d7916442fc163a70795a67d
jkdel <jkd1112@gmail.com>
2015-04-29 16:41:27 +0200

    delete fragment, better loading method (no js)

--- _includes/they.js
+++ _includes/they.js
@@ -1,9 +1,10 @@
 	function init() {
 	  // load contents on start (use jquery instead of liquid because only the content of the article tag should be loaded)
-	  for (var len = col.length, i=0; i<len; ++i) {
-	    $("#"+col[i].name).load(col[i].docs[col[i].docs.length-1]+" article");
-	    col[i].loaded = col[i].docs.length-1;
-	  }
+	  // for (var len = col.length, i=0; i<len; ++i) {
+	  //   $("#"+col[i].name).load(col[i].docs[col[i].docs.length-1]+" article");
+	  //   col[i].loaded = col[i].docs.length-1;
+	  // }
+	  null;
 	}
 
 	function back_to_state(event) {
