ea15ed8cf23f31f9cea44454aa2745fd44bd0763
Julien Delarocque <jkd1112@gmail.com>
2016-01-03 14:06:11 +0100

    some cleanups, no code removal

--- _includes/they.js
+++ _includes/they.js
@@ -1,46 +1,46 @@
-	$(document).ready(function() {
-	  for (var len = col.length, i=0; i<len; ++i) {
-	    col[i].loaded = col[i].docs.length-1;
-	  }
-	  load_gallery();
-	});
-
-	function back_to_state(event) {
-		null;
+$(document).ready(function() {
+	for (var len = col.length, i=0; i<len; ++i) {
+		col[i].loaded = col[i].docs.length-1;
 	}
+	load_gallery();
+});
 
-	function load_and_update(len, collection_index) {
-	    href = col[collection_index].docs[len];
-	    $("#"+col[collection_index].name).load(href+" article", function() {
-	           $('.gallery', this).each(function() {
-	               $(this).magnificPopup({
-	                   delegate: 'a',
-	                   type: 'image',
-	                   tLoading: 'Chargement...',
-	                   mainClass: 'mfp-img-mobile',
-	                   gallery: {
-	                     enabled: true,
-	                     navigateByImgClick: true,
-	                     preload: [0,1],
-	                     tPrev: 'Précédent',
-	                     tNext: 'Suivant',
-	                     tCounter: '<span class="mfp-counter">%curr% sur %total%</span>'
-	                   },
-	                   image: {
-	                     tError: '<a href="%url%">L\'image #%curr%</a> n\'a pas pu être chargée.'
-	                   }
-	               });
-	           });
-						 try {
-							 FB.XFBML.parse();
-						 } catch(ex) {
-							 console.log("couldn't parse FB like");
-						 }
-	   		  });
-	    col[collection_index].loaded = len;
-	    update_arrows(len, collection_index);
-      ga('send', 'pageview', {
-        'page': href,
-        'title': col[collection_index].titles[len]
-      });
-    }
+function back_to_state(event) {
+	null;
+}
+
+function load_and_update(len, collection_index) {
+	href = col[collection_index].docs[len];
+	$("#"+col[collection_index].name).load(href+" article", function() {
+		$('.gallery', this).each(function() {
+			$(this).magnificPopup({
+				delegate: 'a',
+				type: 'image',
+				tLoading: 'Chargement...',
+				mainClass: 'mfp-img-mobile',
+				gallery: {
+					enabled: true,
+					navigateByImgClick: true,
+					preload: [0,1],
+					tPrev: 'Précédent',
+					tNext: 'Suivant',
+					tCounter: '<span class="mfp-counter">%curr% sur %total%</span>'
+				},
+				image: {
+					tError: '<a href="%url%">L\'image #%curr%</a> n\'a pas pu être chargée.'
+				}
+			});
+		});
+		try {
+			FB.XFBML.parse();
+		} catch(ex) {
+			console.log("couldn't parse FB like");
+		}
+	});
+	col[collection_index].loaded = len;
+	update_arrows(len, collection_index);
+	ga('send', 'pageview', {
+		'page': href,
+		'title': col[collection_index].titles[len]
+	});
+}
--- _includes/they_post.js
+++ _includes/they_post.js
@@ -1,72 +1,72 @@
-	function identify() {
-		// need to set loaded _content for the current collection for he arows to work
-		page_col = location.pathname.split("/")[1];
-		for (var len = col.length, i=0; i<len; ++i) {
-			if (col[i].name == page_col) {
-				for (var len2 = col[i].docs.length, j=0; j<len2; ++j) {
-					if (col[i].docs[j] == location.pathname) {
-						break;
-					}
+function identify() {
+	// need to set loaded _content for the current collection for he arows to work
+	page_col = location.pathname.split("/")[1];
+	for (var len = col.length, i=0; i<len; ++i) {
+		if (col[i].name == page_col) {
+			for (var len2 = col[i].docs.length, j=0; j<len2; ++j) {
+				if (col[i].docs[j] == location.pathname) {
+					break;
 				}
-				break;
 			}
+			break;
 		}
-		return [i,j];
 	}
+	return [i,j];
+}
 
-	$(document).ready(function() {
-		ij = identify();
-		col[ij[0]].loaded = ij[1];
-		load_gallery();
-	});
+$(document).ready(function() {
+	ij = identify();
+	col[ij[0]].loaded = ij[1];
+	load_gallery();
+});
 
-	function push_state(href) {
-		history.pushState('', 'New URL: '+href, href);
-	}
+function push_state(href) {
+	history.pushState('', 'New URL: '+href, href);
+}
 
-	function back_to_state(event) {
-		ij = identify();
-		load_and_update(ij[1],ij[0]);
-	}
+function back_to_state(event) {
+	ij = identify();
+	load_and_update(ij[1],ij[0]);
+}
 
-	function load_and_update(len, collection_index) {
-		if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
-			col[collection_index].loaded = len;
-			window.location = col[collection_index].docs[len];
-		} else {
-			href = col[collection_index].docs[len];
-			$("#"+col[collection_index].name).load(href+" article", function() {
-				$('.gallery', this).each(function() {
-					$(this).magnificPopup({
-						delegate: 'a',
-						type: 'image',
-						tLoading: 'Chargement...',
-						mainClass: 'mfp-img-mobile',
-						gallery: {
-							enabled: true,
-							navigateByImgClick: true,
-							preload: [0,1],
-							tPrev: 'Précédent',
-							tNext: 'Suivant',
-							tCounter: '<span class="mfp-counter">%curr% sur %total%</span>'
-						},
-						image: {
-							tError: '<a href="%url%">L\'image #%curr%</a> n\'a pas pu être chargée.'
-						}
-					});
+function load_and_update(len, collection_index) {
+	if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
+		col[collection_index].loaded = len;
+		window.location = col[collection_index].docs[len];
+	} else {
+		href = col[collection_index].docs[len];
+		$("#"+col[collection_index].name).load(href+" article", function() {
+			$('.gallery', this).each(function() {
+				$(this).magnificPopup({
+					delegate: 'a',
+					type: 'image',
+					tLoading: 'Chargement...',
+					mainClass: 'mfp-img-mobile',
+					gallery: {
+						enabled: true,
+						navigateByImgClick: true,
+						preload: [0,1],
+						tPrev: 'Précédent',
+						tNext: 'Suivant',
+						tCounter: '<span class="mfp-counter">%curr% sur %total%</span>'
+					},
+					image: {
+						tError: '<a href="%url%">L\'image #%curr%</a> n\'a pas pu être chargée.'
+					}
 				});
-				try {
-					FB.XFBML.parse();
-				} catch(ex) {
-					console.log("couldn't parse FB like");
-				}
 			});
-			col[collection_index].loaded = len;
-			update_arrows(len, collection_index);
-			push_state(href);
-			ga('send', 'pageview', {
-				'page': href,
-				'title': col[collection_index].titles[len]
-			});
-		}
+			try {
+				FB.XFBML.parse();
+			} catch(ex) {
+				console.log("couldn't parse FB like");
+			}
+		});
+		col[collection_index].loaded = len;
+		update_arrows(len, collection_index);
+		push_state(href);
+		ga('send', 'pageview', {
+			'page': href,
+			'title': col[collection_index].titles[len]
+		});
 	}
+}
